8f43e5b946162c4c0ab0242bd9a30360021ea401
Sriharsha Dodda <sriharsha.d@Technovert.com>
2022-09-01 15:08:28 +0530

    Added feature 2 to main

--- demopy.py
+++ demopy.py
@@ -17,4 +17,7 @@ print(str)
 
 print("trying out feature 11")
 
+print("Adding feature 1 to main")
+
+print("Adding feature 2 to main")
 print("ok, this is something unexpected for the try1 after we did merge")
