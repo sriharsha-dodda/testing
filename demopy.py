6a86f7fbd9e14dfd21459b6399ca4d7c77de6b2c
Sriharsha Dodda <sriharsha.d@Technovert.com>
2022-09-01 12:31:58 +0530

    going back correctly this time

--- demopy.py
+++ demopy.py
@@ -5,34 +5,8 @@ for i in lis:
 
 lis2 = ["abcd", "efgh", "ijkl", "mnop"]
 for j in lis2:
-<<<<<<< HEAD
     print(j, end=":")
 
 lis3 = ["a", "b", "c", "d"]
 for x in lis3:
     print(x, end=":")
-
-
-def triangle(n):
-	k = n - 1
-	for i in range(0, n):
-		for j in range(0, k):
-			print(end=" ")
-		k = k - 1
-		for j in range(0, i+1):
-			print("* ", end="")
-		print("\r")
-n = 5
-triangle(n)
-
-
-def contnum(n):
-	num = 1
-	for i in range(0, n):
-		for j in range(0, i+1):
-			print(num, end=" ")
-			num = num + 1
-		print("\r")
-n = 5
-contnum(n)
-
